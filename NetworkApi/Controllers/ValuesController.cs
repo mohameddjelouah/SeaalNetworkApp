2cc2cafaecfcd3e09b4498421af31153bdfafc13
mohamed djelouah <djelouah16@gmail.com>
2019-07-16 14:44:25 +0100

    changing get but nothing
    try to change get to see multiple results playing with api

--- NetworkApi/Controllers/ValuesController.cs
+++ NetworkApi/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace NetworkApi.Controllers
         {
             //var l = User.Identity.Name;  // to get who is logged in in windows and returning his username
             
-            return new string[] { "value1", "value2" ,User.Identity.Name};
+            return new string[] { "value1", "value2" ,""+User.Identity.Name};
         }
 
         // GET api/values/5
