af24af5c4fe4724f8ea0ba05016c54c7c8a5cdf0
mohamed djelouah <djelouah16@gmail.com>
2019-07-17 11:20:53 +0100

    adding incident table
    add the incident table to database

---
+++ SeaalNetworkDB/dbo/Tables/Incidents.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Incidents]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [IncidentDate] DATETIME2 NOT NULL, 
+    [Direction] NVARCHAR(100) NOT NULL, 
+    [Site] NVARCHAR(200) NOT NULL, 
+    [Nature] NVARCHAR(2000) NOT NULL, 
+    [Operateur] NVARCHAR(50) NOT NULL,
+	[isClotured] INT NOT NULL,
+    [Solution] NVARCHAR(MAX) NULL, 
+    [ClotureDate] DATETIME2 NULL, 
+    
+)
