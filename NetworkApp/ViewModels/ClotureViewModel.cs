8fb5a762bb892a4e3c6d0c59ee7e188ce3e41fe9
m.DJELOUAH <m.DJELOUAH@NB-15-048>
2019-12-29 13:44:37 +0100

    fixing upload in cloture incident

--- NetworkApp/ViewModels/ClotureViewModel.cs
+++ NetworkApp/ViewModels/ClotureViewModel.cs
@@ -264,19 +264,17 @@ namespace NetworkApp.ViewModels
 
             ws.ColumnWidth = 30;
 
-            var solutionrange = ws.Range("D3:H3");
-
             ws.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
             ws.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
             ws.Style.Alignment.WrapText = true;
             ws.Style.Font.FontSize = 13;
 
-            ws.Range("A1:J1").Style.Font.SetBold().Font.FontSize = 15;
+            ws.Range("A1:G1").Style.Font.SetBold().Font.FontSize = 15;
 
 
             //*****************************************************************************************************
             //*****************************************************************************************************
-            ws.Range("G1:H1").Merge();
+            
             //Escribrie en Excel en cada celda
 
             ws.Cell(1, "A").SetValue<string>("Date Incident");
@@ -285,9 +283,7 @@ namespace NetworkApp.ViewModels
             ws.Cell(1, "D").SetValue<string>("Nature");
             ws.Cell(1, "E").SetValue<string>("Origin");
             ws.Cell(1, "F").SetValue<string>("Operateur");
-            ws.Cell(1, "G").SetValue<string>("Solution");
-            ws.Cell(1, "I").SetValue<string>("Date Cloture");
-            ws.Cell(1, "J").SetValue<string>("Ajouter Par");
+            ws.Cell(1, "G").SetValue<string>("Ajouter Par");
 
             //*****************************************************************************************************
             //*****************************************************************************************************
@@ -295,7 +291,7 @@ namespace NetworkApp.ViewModels
             int row = 2;
             foreach (var c in dataincident)
             {
-                ws.Range($"G{row}:H{row}").Merge();
+                
                 
                 ws.Cell(row, "A").SetValue<string>(Convert.ToString(c.IncidentDate.Value.ToString("dd/MM/yyyy")));
                 ws.Cell(row, "B").SetValue<string>(c.Direction.Direction);
@@ -303,9 +299,7 @@ namespace NetworkApp.ViewModels
                 ws.Cell(row, "D").SetValue<string>(c.Nature.Nature);
                 ws.Cell(row, "E").SetValue<string>(c.Origin.Origin);
                 ws.Cell(row, "F").SetValue<string>(c.Operateur?.Operateur);
-                ws.Cell(row, "G").SetValue<string>(c.Solution);
-                ws.Cell(row, "I").SetValue<string>(Convert.ToString(c.ClotureDate.Value.ToString("dd/MM/yyyy")));
-                ws.Cell(row, "J").SetValue<string>(c.AddBy);
+                ws.Cell(row, "G").SetValue<string>(c.AddBy);
 
 
 
